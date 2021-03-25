0a32979356afe8336a006a363fa97f3802ac7ce2
gianca1994 <gianca9405@gmail.com>
2021-03-25 15:42:02 -0300

    Update

--- rolGame/game.java
+++ rolGame/game.java
@@ -1,22 +1,34 @@
 package gameJava.rolGame;
 
+import java.util.Scanner;
+
 
 class Game {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DBWork database = new DBWork();
         Users users = new Users();
+        Utilities util = new Utilities();
+        Scanner scan = new Scanner(System.in);
 
 
         boolean verifyCreateDb = database.createAppDatabase();
         System.out.println("Database connection: " + verifyCreateDb + "\n");
 
-        // PARA REGISTRAR...
-        users.registerUser();
+        util.timer(5);
 
-        // PARA LOGUEAR
-        users.loginUser();
+        System.out.println("Login or register to continue.");
+        System.out.println("Type 1-For login o 2-For register");
 
+        String opt = scan.nextLine();
+        util.clear();
 
+        switch (opt) {
+            case "1" -> users.loginUser();
+            case "2" -> users.registerUser();
+            default -> throw new IllegalStateException("Unexpected value: " + opt);
+        }
     }
+
+
 }
